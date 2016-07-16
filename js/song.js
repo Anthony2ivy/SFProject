61782c042d1eee35c73d6d1b0e3bf2bdfaf34872
pandaiivy <pandaiivy@localhost>
2016-07-16 21:05:21 +0800

    最新最新的

--- js/song.js
+++ js/song.js
@@ -1,9 +1,10 @@
-var table1 = new Array(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120,150);
+var table1 = new Array(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120);//传表格的值
+var table2 = new Array(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 900, 800);传表格的值
 var t = new Array();
 var d = new Array();
-//表
 
-function createTable(name1, name2, j) {
+function createTable(name1, name2, j, tableV) {
+
 	var table = document.getElementsByClassName("data")[j];
 	var tr = document.createElement("tr");
 	table.appendChild(tr);
@@ -25,13 +26,13 @@ function createTable(name1, name2, j) {
 	tr2.appendChild(td2);
 	for(var i = 0; i < 12; i++) {
 		d[i] = document.createElement("td");
-		d[i].innerHTML = table1[i];
+		d[i].innerHTML = tableV[i];
 		tr2.appendChild(d[i]);
 	}
 }
-	//画图的函数
-function fillchart(chart, valuepic, tree, max, min) {
-	mistakeStep = 13; //允许点击的误差范围
+//画图的函数
+function fillchart(chart, valuepic, tree, max, min, name1, name2) {
+	mistakeStep = 20; //允许点击的误差范围
 	var point = new Map();
 	var pointValue = new Map(); //值
 	months = new Array(); //横坐标
@@ -48,10 +49,20 @@ function fillchart(chart, valuepic, tree, max, min) {
 
 	c = chart;
 	ctx = c.getContext("2d");
+	//消除锯齿
+
+	let width = c.width,
+		height = c.height;
+	if(window.devicePixelRatio) {
+		c.style.width = width + "px";
+		c.style.height = height + "px";
+		c.height = height * window.devicePixelRatio;
+		c.width = width * window.devicePixelRatio;
+		ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+	}
 	ctx.save();
 	ctx.beginPath();
-	ctx.strokeStyle = "rgb(68,67,65)";
-
+	ctx.fillStyle = "rgb(207,207,207)";
 	ctx.lineWidth = 0.1;
 	ctx.translate(100, 200);
 	ctx.moveTo(0, 0);
@@ -60,7 +71,8 @@ function fillchart(chart, valuepic, tree, max, min) {
 	ctx.moveTo(0, 0);
 	ctx.lineTo(0, -150); // Y axis
 	ctx.moveTo(0, 0);
-	ctx.fillStyle = "rgb(207,207,207)";
+
+	//表
 	//纵坐标
 	var Xpos; //x轴上左边标明数值的位置 自定义
 	var Ypos = 5; //y 轴上起点
@@ -74,23 +86,24 @@ function fillchart(chart, valuepic, tree, max, min) {
 		} else if(parseFloat(Wert) >= 100 && parseFloat(Wert) < 1000) {
 			Xpos = -28;
 		} else if(parseFloat(Wert) >= 1000) {
-			Xpos = -35;
+			Xpos = -36;
 		} else if(parseFloat(Wert) < 10) {
 			Xpos = -15;
 		}
 		ctx.fillText(Wert, Xpos, Ypos + i * Ystep);
 	}
-	ctx.fillText("个数", -40, -175);
+	ctx.fillStyle = "#E4E4E4";
+	ctx.fillText(name1, -40, -175);
 	ctx.fillText("月份", 270, 10);
-	ctx.fillText("运单量", 125, -185);
-	ctx.strokeStyle = "rgb(207,207,207)";
+	ctx.fillText(name2, 125, -190);
+	ctx.strokeStyle = "#E4E4E4";
 	for(var i = 1; i < 6; i++) {
 		ctx.moveTo(0, i * (-30));
 		ctx.lineTo(5, i * (-30));
 		ctx.stroke();
 	} //Y axis
 	ctx.moveTo(0, 0);
-	ctx.lineWidth = 2;
+	
 
 	for(var i = 1; i < 13; i++) {
 		ctx.moveTo(i * 20, 0);
@@ -100,7 +113,7 @@ function fillchart(chart, valuepic, tree, max, min) {
 
 	ctx.stroke();
 	ctx.moveTo(0, 0);
-
+	ctx.lineWidth = 2;
 	//取得表中的数据
 
 	for(var i = 0; i < 13; i++) {
@@ -111,7 +124,7 @@ function fillchart(chart, valuepic, tree, max, min) {
 			var m = parseFloat(-300 / (2 * parseFloat(max)));
 			yWert[i - 1] = parseFloat(m * dataset[i - 1]);
 			pointValue.set(months[i - 1], dataset[i - 1]);
-			point.set(months[i - 1], parseInt(yWert[i - 1] + 200));
+			point.set(months[i - 1], parseFloat(yWert[i - 1] + 200));
 
 		}
 	}
@@ -129,25 +142,25 @@ function fillchart(chart, valuepic, tree, max, min) {
 		var x, y;
 		var absolutePos = c.getBoundingClientRect();
 		return {
-			x: x - absolutePos.left * (c.width / absolutePos.width),
-			y: y - absolutePos.top * (c.height / absolutePos.height)
+			x: x - absolutePos.left * (c.width / absolutePos.width)/window.devicePixelRatio,
+			y: y - absolutePos.top * (c.height / absolutePos.height)/window.devicePixelRatio
 		};
 	}
 	//事件监听
 	c.addEventListener('mousemove', function(e) {
 
 		p = getEventPosition(e, e.clientX, e.clientY);
-		var x = parseInt(p.x);
-		var y = parseInt(p.y);
+		var x = parseFloat(p.x);
+		var y = parseFloat(p.y);
 		//	judgeClick(x,y);
 
 		for(var i = 1; i < 13; i++) {
-			if(parseInt(Math.abs(x - months[i - 1])) < parseInt(mistakeStep)) {
+			if(parseFloat(Math.abs(x - months[i - 1])) < parseFloat(mistakeStep)) {
 				for(var j = 1; j < 13; j++) {
 					//把 map 里的 key 值转为数字
-					var valueY = Math.abs(y - parseInt(point.get(months[i - 1])));
-					if(parseInt(valueY) < parseInt(mistakeStep)) {
-						loadPic1(months[i - 1] - 8, point.get(months[i - 1]) - 20, pointValue.get(months[i - 1]));
+					var valueY = Math.abs(y - parseFloat(point.get(months[i - 1])));
+					if(parseFloat(valueY) < parseFloat(mistakeStep)) {
+						loadPic1(months[i - 1] - 17, point.get(months[i - 1]) - 20, pointValue.get(months[i - 1]));
 						break;
 					}
 					break;
@@ -158,14 +171,15 @@ function fillchart(chart, valuepic, tree, max, min) {
 	//绘制起泡图片
 	function drawPic1(x, y, pic, value) {
 		part = valuepic;
-		y = parseInt(y) + 44;
+		y = parseFloat(y) + 34;
 		part.style.top = y + "px";
 		part.style.left = x + "px";
-		part.style.width = "30px";
-		part.style.height = "30px";
+		part.style.width = "36px";
+		part.style.height = "36px";
 		part.innerHTML = value;
+		part.style.color = "rgb(67,66,64)";
 		part.style.textAlign = "center";
-		part.style.lineHeight = "28px";
+		part.style.lineHeight = "30px";
 	}
 	//气泡图片加载
 	function loadPic1(x, y, value) {
@@ -187,12 +201,12 @@ function fillchart(chart, valuepic, tree, max, min) {
 	} else {}
 }
 
-createTable("月份","运单量",0);
-createTable("月份","运单金额",1);
-createTable("月份","平均重量",2);
-createTable("月份","平均折扣",3);
+createTable("月份", "运单量", 0, table2);
+createTable("月份", "运单量", 1, table1);
+createTable("月份", "运单量", 2, table2);
+createTable("月份", "运单量", 3, table1);
 
-fillchart(document.getElementsByClassName("chart")[0], document.getElementsByClassName("valuePic")[0], document.getElementsByClassName("value")[0].getElementsByTagName("td"), 1000, 0);
-fillchart(document.getElementsByClassName("chart")[1], document.getElementsByClassName("valuePic")[1], document.getElementsByClassName("value")[1].getElementsByTagName("td"), 100, 0);
-fillchart(document.getElementsByClassName("chart")[2], document.getElementsByClassName("valuePic")[2], document.getElementsByClassName("value")[2].getElementsByTagName("td"), 1000, 0);
-fillchart(document.getElementsByClassName("chart")[3], document.getElementsByClassName("valuePic")[3], document.getElementsByClassName("value")[3].getElementsByTagName("td"), 100, 0);
+fillchart(document.getElementsByClassName("chart")[0], document.getElementsByClassName("valuePic")[0], document.getElementsByClassName("value")[0].getElementsByTagName("td"), 10000, 0, "个数", "运单量");
+fillchart(document.getElementsByClassName("chart")[1], document.getElementsByClassName("valuePic")[1], document.getElementsByClassName("value")[1].getElementsByTagName("td"), 100, 0, "金额", "运单量");
+fillchart(document.getElementsByClassName("chart")[2], document.getElementsByClassName("valuePic")[2], document.getElementsByClassName("value")[2].getElementsByTagName("td"), 1000, 0, "重量", "平均重量");
+fillchart(document.getElementsByClassName("chart")[3], document.getElementsByClassName("valuePic")[3], document.getElementsByClassName("value")[3].getElementsByTagName("td"), 100, 0, "折数", "折扣");
