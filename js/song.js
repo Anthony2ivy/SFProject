d8e62e36bff8a346a27901b983eaf87bf0269b2b
pandaiivy <pandaiivy@localhost>
2016-07-17 13:36:21 +0800

    final

--- js/song.js
+++ js/song.js
@@ -1,37 +1,54 @@
 var table1 = new Array(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120);//传表格的值
-var table2 = new Array(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 900, 800);传表格的值
-var t = new Array();
-var d = new Array();
+var table2 = new Array(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 900, 800);//传表格的值
+var table3 = new Array(22,33,44,55,66,77,88,9,99,102,23,12);
+var table4 = new Array(234,345,456,567,678,789,890,123,234,452,642,123);
+var t = new Array();//放后面表的月份的对象
+var d = new Array();//放表数值的对象
 
-function createTable(name1, name2, j, tableV) {
 
-	var table = document.getElementsByClassName("data")[j];
-	var tr = document.createElement("tr");
-	table.appendChild(tr);
-	var td = document.createElement("td");
-	td.className = "figure";
-	td.innerHTML = name1;
-	tr.appendChild(td);
-	for(var i = 0; i < 12; i++) {
-		t[i] = document.createElement("td");
-		t[i].innerHTML = i + 1;
-		tr.appendChild(t[i]);
+//取最小大于最大值的10的倍数或取最大小于最小值的10的倍数
+function firstNum(max,k){
+	var q;
+	for (var i = 0; max>0;i++){
+		if(max <=10&&max>=1&&i!=0){
+			q=max;
+		}
+		else if(max<=10&&max>=1&&i==0){
+			return 0;
+		}
+		else if(max%100==0&&i==0){
+			return max;
+		}
+		max = parseInt(max/10);
 	}
-	var tr2 = document.createElement("tr");
-	tr2.className = "value";
-	table.appendChild(tr2);
-	var td2 = document.createElement("td");
-	td2.className = "figure";
-	td2.innerHTML = name2;
-	tr2.appendChild(td2);
-	for(var i = 0; i < 12; i++) {
-		d[i] = document.createElement("td");
-		d[i].innerHTML = tableV[i];
-		tr2.appendChild(d[i]);
-	}
+	//k=1就是取最小大于
+	//k=0取最大小于
+	result = (q+k)*Math.pow(10,i-1);
+	return result;
 }
+//比较因子
+function sortNum(a,b){
+	return a-b;
+}
+//数值排序 确定最大值最小值
+function setAxis(arr){
+	var sortedArr = new Array();
+	sortedArr = arr.slice(0);
+	sortedArr.sort(sortNum);
+	var max = sortedArr[sortedArr.length-1];
+	max = firstNum(max,1);
+	var min = sortedArr[0];
+	min = firstNum(min,0);
+	var step = (max-min)/5;
+	 arr.push(max);
+	 arr.push(min);
+	 arr.push(step);
+	 return arr;
+	 
+}
+
 //画图的函数
-function fillchart(chart, valuepic, tree, max, min, name1, name2) {
+function fillchart(chart, valuepic, arr, name1, name2) {
 	mistakeStep = 20; //允许点击的误差范围
 	var point = new Map();
 	var pointValue = new Map(); //值
@@ -77,7 +94,10 @@ function fillchart(chart, valuepic, tree, max, min, name1, name2) {
 	var Xpos; //x轴上左边标明数值的位置 自定义
 	var Ypos = 5; //y 轴上起点
 	var Ystep = -30; //y 轴上间隔
-	var step = parseFloat(max - min) / 5;
+	var step = arr.pop();
+	var min = arr.pop();
+	var max = arr.pop();
+//	alert(step+" "+arr.pop()+" "+arr.pop());
 	for(var i = 0; i < 6; i++) {
 		//根据横纵坐标大小来设置它在 X 轴上的位置
 		var Wert = min + i * step;
@@ -95,7 +115,9 @@ function fillchart(chart, valuepic, tree, max, min, name1, name2) {
 	ctx.fillStyle = "#E4E4E4";
 	ctx.fillText(name1, -40, -175);
 	ctx.fillText("月份", 270, 10);
-	ctx.fillText(name2, 125, -190);
+	ctx.fillStyle = "#FFFFFF";
+	ctx.font = "15px Calibri";
+	ctx.fillText(name2, 125, -180);
 	ctx.strokeStyle = "#E4E4E4";
 	for(var i = 1; i < 6; i++) {
 		ctx.moveTo(0, i * (-30));
@@ -119,7 +141,7 @@ function fillchart(chart, valuepic, tree, max, min, name1, name2) {
 	for(var i = 0; i < 13; i++) {
 		if(i != 0) {
 
-			dataset[i - 1] = tree[i].innerText; //获得表中的数据
+			dataset[i - 1] = arr[i-1] //获得表中的数据
 			//根据坐标轴范围处理数据
 			var m = parseFloat(-300 / (2 * parseFloat(max)));
 			yWert[i - 1] = parseFloat(m * dataset[i - 1]);
@@ -152,7 +174,6 @@ function fillchart(chart, valuepic, tree, max, min, name1, name2) {
 		p = getEventPosition(e, e.clientX, e.clientY);
 		var x = parseFloat(p.x);
 		var y = parseFloat(p.y);
-		//	judgeClick(x,y);
 
 		for(var i = 1; i < 13; i++) {
 			if(parseFloat(Math.abs(x - months[i - 1])) < parseFloat(mistakeStep)) {
@@ -171,6 +192,8 @@ function fillchart(chart, valuepic, tree, max, min, name1, name2) {
 	//绘制起泡图片
 	function drawPic1(x, y, pic, value) {
 		part = valuepic;
+		//数值比较少的时候气泡比较小
+		if(parseFloat(value)<=1000){
 		y = parseFloat(y) + 34;
 		part.style.top = y + "px";
 		part.style.left = x + "px";
@@ -180,6 +203,11 @@ function fillchart(chart, valuepic, tree, max, min, name1, name2) {
 		part.style.color = "rgb(67,66,64)";
 		part.style.textAlign = "center";
 		part.style.lineHeight = "30px";
+		}
+		//数值较大的时候气泡变大
+		else if (parseFloat(value)>1000){
+			
+		}
 	}
 	//气泡图片加载
 	function loadPic1(x, y, value) {
@@ -200,13 +228,41 @@ function fillchart(chart, valuepic, tree, max, min, name1, name2) {
 		window.attachEvent('onload', load);
 	} else {}
 }
+function createTable(name1, name2, j, tableV) {
 
-createTable("月份", "运单量", 0, table2);
-createTable("月份", "运单量", 1, table1);
-createTable("月份", "运单量", 2, table2);
-createTable("月份", "运单量", 3, table1);
+	var table = document.getElementsByClassName("data")[j];
+	var tr = document.createElement("tr");
+	table.appendChild(tr);
+	var td = document.createElement("td");
+	td.className = "figure";
+	td.innerHTML = "月份";
+	tr.appendChild(td);
+	for(var i = 0; i < 12; i++) {
+		t[i] = document.createElement("td");
+		t[i].innerHTML = i + 1;
+		tr.appendChild(t[i]);
+	}
+	var tr2 = document.createElement("tr");
+	tr2.className = "value";
+	table.appendChild(tr2);
+	var td2 = document.createElement("td");
+	td2.className = "figure";
+	td2.innerHTML = "运单量";
+	tr2.appendChild(td2);
+	for(var i = 0; i < 12; i++) {
+		d[i] = document.createElement("td");
+		d[i].innerHTML = tableV[i];
+		tr2.appendChild(d[i]);
+	}
+	tableV = setAxis(tableV);
+//	for(var i = 0;i<tableV.length;i++){
+//		alert(tableV[i]);
+//	}
+fillchart(document.getElementsByClassName("chart")[j], document.getElementsByClassName("valuePic")[j], tableV, name1, name2);
+}
+createTable("个数", "运单量", 0, table2);
+createTable("金额", "运单量", 1, table1);
+createTable("重量", "运单量", 2, table4);
+createTable("折扣", "运单量", 3, table3);
 
-fillchart(document.getElementsByClassName("chart")[0], document.getElementsByClassName("valuePic")[0], document.getElementsByClassName("value")[0].getElementsByTagName("td"), 10000, 0, "个数", "运单量");
-fillchart(document.getElementsByClassName("chart")[1], document.getElementsByClassName("valuePic")[1], document.getElementsByClassName("value")[1].getElementsByTagName("td"), 100, 0, "金额", "运单量");
-fillchart(document.getElementsByClassName("chart")[2], document.getElementsByClassName("valuePic")[2], document.getElementsByClassName("value")[2].getElementsByTagName("td"), 1000, 0, "重量", "平均重量");
-fillchart(document.getElementsByClassName("chart")[3], document.getElementsByClassName("valuePic")[3], document.getElementsByClassName("value")[3].getElementsByTagName("td"), 100, 0, "折数", "折扣");
+
